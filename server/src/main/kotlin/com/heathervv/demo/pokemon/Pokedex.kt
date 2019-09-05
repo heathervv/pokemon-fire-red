264c6f54855944895744676398b5759e8210999b
Heather Vandervecht <heathervandervecht@gmail.com>
2019-09-05 10:37:59 -0400

    Add Pokedex enum class for starters

---
+++ server/src/main/kotlin/com/heathervv/demo/pokemon/Pokedex.kt
@@ -0,0 +1,13 @@
+package com.heathervv.demo.pokemon
+
+enum class Pokedex(val id: Int) {
+    BULBASAUR(1),
+    IVYSAUR(2),
+    VENUSAUR(3),
+    CHARMANDER(4),
+    CHARMELEON(5),
+    CHARIZARD(6),
+    SQUIRTLE(7),
+    WARTORTLE(7),
+    BLASTOISE(7)
+}
--- server/src/main/kotlin/com/heathervv/demo/pokemon/PokemonController.kt
+++ server/src/main/kotlin/com/heathervv/demo/pokemon/PokemonController.kt
@@ -2,7 +2,6 @@ package com.heathervv.demo.pokemon
 
 import org.springframework.web.bind.annotation.*
 
-//TODO store national pokedex ids in enum
 //TODO cache pokeApi responses
 
 @RestController
@@ -10,15 +9,17 @@ class PokemonController(private val client: PokemonApiClient) {
 
     @GetMapping("/api/pokemon/starter", produces = ["application/json"])
     fun starter(@RequestHeader("User-Agent") userAgent: String): List<PokemonResponse> {
-        val bulbasaur = client.getPokemon(userAgent, 1)
-        val charmander = client.getPokemon(userAgent, 4)
-        val squirtle = client.getPokemon(userAgent, 7)
 
-        return listOf(bulbasaur, charmander, squirtle)
+        val starters = listOf(Pokedex.BULBASAUR.id, Pokedex.CHARMANDER.id, Pokedex.SQUIRTLE.id)
+
+        return starters.map { id ->
+            client.getPokemon(userAgent, id)
+        }
     }
 
     @PostMapping("/api/pokemon/starter", produces = ["application/json"])
     fun chooseStarter(@RequestBody starter: String): String {
+
         // TODO store chosen starter in DB or something
         return starter
     }
